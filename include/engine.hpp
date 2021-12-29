761c6786ac8c569ceeb81bb369086a68826f52b0
freire-guido <freireguido@outlook.com>
2021-12-29 13:06:59 -0300

    point gravitation

--- include/engine.hpp
+++ include/engine.hpp
@@ -3,14 +3,20 @@
 #include "particle.hpp"
 #include "math.hpp"
 
-void gravitate(Particle&a, Particle& b, float cons = 1){
+void gravitate(Particle& a, Particle& b, float cons = 1) {
     const sf::Vector2f direction = b.position - a.position;
     const float radiusSq = ((a.mass + b.mass)*(a.mass + b.mass));
     a.accelerate(direction*cons*b.mass / radiusSq);
     b.accelerate(-direction*cons*a.mass / radiusSq);
 }
 
-void collide(Particle& a, Particle& b){
+void gravitate(Particle& particle, sf::Vector2f point, float cons = 1) {
+    const sf::Vector2f direction = point - particle.position;
+    const float radiusSq = 4*particle.mass*particle.mass; // Point mass equal to particle mass
+    particle.accelerate(direction*cons*particle.mass / radiusSq);
+}
+
+void collide(Particle& a, Particle& b) {
     const sf::Vector2f direction = b.position - a.position;
     const float constant = dotProduct(a.velocity - b.velocity, a.position - b.position) / (length(a.position - b.position)*length(a.position - b.position));
     a.speed(2.0f*direction*b.mass*constant / (b.mass + a.mass));
@@ -31,12 +37,14 @@ struct Engine {
     }
     void update(float dt) {
         for (int i = 0; i < particles.size(); i++) {
+            particles[i].acceleration = {0, 0};
             for (int j = i + 1; j < particles.size(); j++) {
                 if (areColliding(particles[i], particles[j])) {
                     collide(particles[i], particles[j]);
                 }
-                gravitate(particles[i], particles[j], 0.001);
+                gravitate(particles[i], particles[j], 0.01);
             }
+            gravitate(particles[i], {958, 520}, 0.1);
             particles[i].update(dt);
         }
     }
