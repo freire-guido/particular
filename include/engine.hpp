d5e184491875de5f1c8f90d0e45c9c1ee2268c4a
freire-guido <freireguido@outlook.com>
2021-12-29 13:19:06 -0300

    remove inherent acceleration

--- include/engine.hpp
+++ include/engine.hpp
@@ -3,17 +3,17 @@
 #include "particle.hpp"
 #include "math.hpp"
 
-void gravitate(Particle& a, Particle& b, float cons = 1) {
+void gravitate(Particle& a, Particle& b, float dt = 1, float cons = 1) {
     const sf::Vector2f direction = b.position - a.position;
     const float radiusSq = ((a.mass + b.mass)*(a.mass + b.mass));
-    a.accelerate(direction*cons*b.mass / radiusSq);
-    b.accelerate(-direction*cons*a.mass / radiusSq);
+    a.speed(dt*direction*cons*b.mass / radiusSq);
+    b.speed(-dt*direction*cons*a.mass / radiusSq);
 }
 
-void gravitate(Particle& particle, sf::Vector2f point, float cons = 1) {
+void gravitate(Particle& particle, sf::Vector2f point, float dt = 1, float cons = 1) {
     const sf::Vector2f direction = point - particle.position;
     const float radiusSq = 4*particle.mass*particle.mass; // Point mass equal to particle mass
-    particle.accelerate(direction*cons*particle.mass / radiusSq);
+    particle.speed(dt*direction*cons*particle.mass / radiusSq);
 }
 
 void collide(Particle& a, Particle& b) {
@@ -37,14 +37,13 @@ struct Engine {
     }
     void update(float dt) {
         for (int i = 0; i < particles.size(); i++) {
-            particles[i].acceleration = {0, 0};
             for (int j = i + 1; j < particles.size(); j++) {
                 if (areColliding(particles[i], particles[j])) {
                     collide(particles[i], particles[j]);
                 }
-                gravitate(particles[i], particles[j], 0.01);
+                gravitate(particles[i], particles[j], dt, 0.01);
             }
-            gravitate(particles[i], {958, 520}, 0.1);
+            gravitate(particles[i], {958, 520}, dt, 0.1);
             particles[i].update(dt);
         }
     }
--- include/particle.hpp
+++ include/particle.hpp
@@ -1,14 +1,12 @@
 #include <SFML/System/Vector2.hpp>
 
 struct Particle {
-    sf::Vector2f position, velocity, acceleration;
+    sf::Vector2f position, velocity;
     float mass; // Synonymous with radius in context
     Particle(sf::Vector2f p, float m): position{p}, mass{m} {}
     Particle(sf::Vector2f p, sf::Vector2f v, float m): position{p}, velocity{v}, mass{m} {}
-    void accelerate(sf::Vector2f a) { acceleration += a; }
     void speed(sf::Vector2f v) { velocity += v; }
     void update(float dt) {
-        velocity += acceleration*dt;
         position += velocity*dt;
     }
 };
