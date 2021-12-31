a9a9d134b110f142363def6c32c0fb0887c4ebe6
freire-guido <freireguido@outlook.com>
2021-12-31 19:27:23 -0300

    include atom functionality

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,4 +1,6 @@
 #include <SFML/System/Vector2.hpp>
+#include <vector>
+#include "math.h"
 
 struct Particle {
     sf::Vector2f position, velocity;
@@ -8,5 +10,26 @@ struct Particle {
     void speed(sf::Vector2f v) { velocity += v; }
     void update(float dt) {
         position += velocity*dt;
+    }
+};
+
+struct Atom {
+    std::vector<Particle> electrons;
+    Particle nucleus;
+    std::vector<Particle*> particles;
+    Atom(sf::Vector2f p, float m): nucleus{p, m} {
+        particles.push_back(&nucleus);
+        for (int i = 0; i < m; i++) {
+            sf::Vector2f offset(2*m*cos(2*i*M_PI / m), 2*m*sin(2*i*M_PI / m));
+            electrons.push_back(Particle(p + offset, sf::Vector2f(offset.y, -offset.x) / length(offset), 1));
+            particles.push_back(&electrons[i]);
+        }
+    }
+    void speed(sf::Vector2f v) { nucleus.speed(v); }
+    void update(float dt){
+        nucleus.position += nucleus.velocity*dt;
+        for (Particle& electron: electrons) {
+            electron.position += nucleus.velocity*dt;
+        }
     }
 };
